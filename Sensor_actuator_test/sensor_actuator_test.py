082be9a7212edb3bf713ed88415c302ffb355a19
LAB_Lau_Lauridsen <lau.lauridsen@hotmail.com>
2024-04-05 14:27:14 +0200

    Update sensor_actuator_test.py

--- Sensor_actuator_test/sensor_actuator_test.py
+++ Sensor_actuator_test/sensor_actuator_test.py
@@ -1,6 +1,15 @@
+"""
+04 may 2024
+Author: Lau Lauridsen
+
+Test modbus communication in smart water infastructur lab at Aalborg university department of Electronic Systems
+"""
+
 from pyModbusTCP.client import ModbusClient
+import numpy as np
 import time
-ip_addr = '192.168.100.43'
+
+ip_addr = '192.168.100.43'  #Module IP adress
 
 MB = ModbusClient(host=ip_addr, port=502, auto_open=True)
 time.sleep(3)
@@ -9,17 +18,22 @@ if MB.open():
 else:
     print("Modbus not open")
 
-MB.local.write_multipe_registers(5,100*100)
+data = MB.read_input_registers(1,18)
+print(data)
+
+print(MB.read_input_registers(1,1)[0])
+print(MB.read_input_registers(2,1)[0])
+print(MB.read_input_registers(3,1)[0])
+print(MB.read_input_registers(4,1)[0])
+print(MB.read_input_registers(5,1)[0])
+print(MB.read_input_registers(6,1)[0])
+print(MB.read_input_registers(7,1)[0])
+
+pump_setting1 = np.array([0, 50*100, 0])
+pump_setting2 = np.array([0, 0, 0])
+
+MB.write_multiple_registers(5,list(pump_setting1))
 time.sleep(5) # [s]
-MB.write_multipe_registers(5,0)
+MB.write_multiple_registers(5,list(pump_setting2))
 time.sleep(5)
 
-MB.write_multipe_registers(5,100*100)
-time.sleep(5)
-MB.write_multipe_registers(5,0)
-time.sleep(5)
-
-MB.write_multipe_registers(5,100*100)
-time.sleep(5)
-MB.write_multipe_registers(5,0)
-time.sleep(5)

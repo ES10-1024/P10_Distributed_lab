28b68fa0a23b3b0b74f95df9a8f624379e827839
LauLauridsen <lau.lauridsen@hotmail.com>
2024-04-09 09:59:59 +0200

    Changes in main files

---
+++ pump_station/low_level_settings.py
@@ -0,0 +1,15 @@
+settings_pump1 = {
+    "sampletime":1,
+    "proportiaonl_gain":1,
+    "integral_gain":1,
+    "ip_pump":"192.111",
+    "index_pump":5,
+    "index_pump_tank":5,
+    "ip_tower":'192.111',
+    "index_tower_tank":6,
+    "ip_pipe":'192.',
+    "index_flow_pipe":5,
+    "tank_min":11,
+    "tank_max":11
+}
+
--- pump_station/main.py
+++ pump_station/main.py
@@ -1,48 +1,52 @@
 import time
 import multiprocessing
 from pyModbusTCP.client import ModbusClient
+from low_level_settings import settings_pump1
 
 
-def low_level_controller(sample_time: int, proportiaonl_gain: float, integral_gain: float, ip_pump: str, index_pump: int, index_pump_tank: int, ip_tower: str, index_tower_tank: int, ip_pipe: str, index_flow_pipe,  tank_min: float, tank_max: float,q):
+def low_level_controller(settings_pump: dict, q):
     #q is queue where new refrence can be put
     
     last_sample_time = time.time()
-    ref=0
+    ref = 0
 
-    MB_flow = ModbusClient(host=ip_pipe, port=502, auto_open=True)
-    MB_pump = ModbusClient(host=ip_pump, port=502, auto_open=True)
-    MB_tower = ModbusClient(host=ip_tower, port=502, auto_open=True)
+    MB_flow = ModbusClient(host = settings_pump['ip_pipe'], port = 502, auto_open = True)
+    MB_pump = ModbusClient(host = settings_pump['ip_pump'], port = 502, auto_open=True)
+    MB_tower = ModbusClient(settings_pump['ip_tower'], port = 502, auto_open = True)
 
     while True:
-        sleep_time = sample_time - (time.time()-last_sample_time)
+        sleep_time = settings_pump['sampetime'] - (time.time() - last_sample_time)
         time.sleep(sleep_time)
         last_sample_time = time.time()      #unix time 
 
-        flow = MB_flow.read_input_registers(index_flow_pipe,1)
+        flow = MB_flow.read_input_registers(settings_pump['index_flow_pipe'], 1)
         try:
             ref = q.get_nowait()
         except:
             pass
 
-        error = ref-flow
+        error = ref - flow
+        print(error)
         #Insert PID controller
-        pump_precentage = 0
+        pump_precentage = 50
 
         #Perform supervisory level control and output actuation if ok
-        pump_tank_level = MB_pump.read_input_registers(index_pump_tank,1)
-        tower_tank_level = MB_tower.read_input_registers(index_tower_tank,1)
-        if(pump_tank_level < tank_min or tower_tank_level > tank_max):
-            MB_pump.write_multiple_registers(index_pump, 0)     #Turn off pump
+        pump_tank_level = MB_pump.read_input_registers(settings_pump['index_pump_tank'], 1)
+        tower_tank_level = MB_tower.read_input_registers(settings_pump['index_tower_tank'], 1)
+        if(pump_tank_level < settings_pump['tank_min'] or tower_tank_level > settings_pump['tank_max']):
+            MB_pump.write_multiple_registers(settings_pump['index_pump'], 0)     #Turn off pump
         else:
-            MB_pump.write_multiple_registers(index_pump, pump_precentage)
+            MB_pump.write_multiple_registers(settings_pump['index_pump'], pump_precentage)
 
 
 if __name__ == '__main__':
+    print("hallow world")
     refence_queue = multiprocessing.Queue(1)
-    low_level_control_process = multiprocessing.Process(target=low_level_controller,args=())
+    low_level_control_process = multiprocessing.Process(target = low_level_controller,args = (settings_pump1,refence_queue))
 
 while True:
     #Perform high level control
-        
+    print("hi")
+    time.sleep(5)
 
     

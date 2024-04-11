a1031850c38c5da838c4d56a4d97cae6492911da
LAB_Lau_Lauridsen <lau.lauridsen@hotmail.com>
2024-04-11 11:49:44 +0200

    Low level

--- pump_station/low_level_settings.py
+++ pump_station/low_level_settings.py
@@ -1,7 +1,29 @@
-settings_pump1 = {
+settings_pump1 = {  #Module 41
     "sampletime":1,
-    "proportiaonl_gain":1,
-    "integral_gain":1,
+    "proportional_gain":6.5,
+    "integral_gain":3.5,
+    "initial_integral_value":50/3.5,
+    "lower_saturation_limit":0,
+    "upper_saturation_limit":100,
+    "ip_pump":"192.168.100.41",
+    "register_pump":7,
+    "register_pump_tank":4,
+    "ip_tower":'192.168.100.34',
+    "register_tower_tank":7,
+    "ip_pipe":'192.168.100.20',
+    "register_flow_pipe":1,
+    "pump_tank_min":100,
+    "consumer_tank_max":575
+}
+
+
+settings_pump2 = { #Module 43
+    "sampletime":1,
+    "proportional_gain":16,
+    "integral_gain":3,
+    "initial_integral_value":44/3,
+    "lower_saturation_limit":0,
+    "upper_saturation_limit":100,
     "ip_pump":"192.168.100.43",
     "register_pump":7,
     "register_pump_tank":4,
@@ -9,7 +31,7 @@ settings_pump1 = {
     "register_tower_tank":7,
     "ip_pipe":'192.168.100.20',
     "register_flow_pipe":2,
-    "pump_tank_min":60,
+    "pump_tank_min":100,
     "consumer_tank_max":575
 }
 

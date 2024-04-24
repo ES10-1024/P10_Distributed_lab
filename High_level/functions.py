89b5fcb2d1bbfa0254531c718c4c2b536ca73d8e
LauLauridsen <lau.lauridsen@hotmail.com>
2024-04-24 14:23:39 +0200

    Added some great comments

--- High_level/functions.py
+++ High_level/functions.py
@@ -6,79 +6,62 @@ def local_optimiser(hour : int, water_height : float, z: np.array, rho : float, 
 
 class ADMM_optimiser_WDN:
     def __init__(self, conn1, conn2, N_iterations : int,  N_vary_rho: int, stakeholder: int):
-        self.conn1 = conn1
-        self.conn2 = conn2
-        self.N_vary_rho = N_vary_rho
-        self.stakeholder = stakeholder
-        self.N_iterations = N_iterations
-        self.N_c = 24
-        self.N_s = 3
-        self.N_q = 2
+        self.conn1 = conn1      #TCP connection
+        self.conn2 = conn2      #TCP connection
+        self.N_vary_rho = N_vary_rho        #Number of iterations with varying rho
+        self.stakeholder = stakeholder      #My stakeholder id
+        self.N_iterations = N_iterations    #Number of ADMM iterations
+        self.N_c = 24   #Control horizon
+        self.N_s = 3    #Number of stakeholders
+        self.N_q = 2    #number of pumps
         
-        self.rho = 2
-        self.mu = 10
-        self.tau = 2
+        self.rho = 2    #Initial
+        self.mu = 10    #Vary rho algorithm parameter
+        self.tau = 2    #Vary rho algorithm parameter
 
-        self.z=np.zeros(self.N_c*self.N_q) #ADMM
+        self.z=np.zeros(self.N_c*self.N_q) #Initialization ADMM
 
 
     def optimise(self, hour : int , water_height: float):
-        self.lambda_i = np.zeros(self.N_c*self.N_q) #ADMM
-        self.x_bar = np.zeros(self.N_c*self.N_q) #ADMM
+        self.lambda_i = np.zeros(self.N_c*self.N_q) #ADMM Initialisation
+        self.x_bar = np.zeros(self.N_c*self.N_q) #ADMM Initialisation
         
         #Timeshift initial guess
         self.z = np.roll(self.z,1)
         self.z[-1] = self.z[-2]    
 
-
         #Solve problem
         for k in range(0,self.N_iterations):
             print("Iteration", k)
+            #Solve local problem
             try: 
-                self.x_i = local_optimiser(hour, water_height, self.z, self.rho, self.stakeholder) #Local optimisation (equation a)
+                self.x_i = local_optimiser(hour, water_height, self.z, self.rho, self.stakeholder) 
             except:
                 self.x_i = 3/4*self.x_i + 1/4*self.z
                 print("Local optimisation failed")
 
             ### BEGIN ADMM
-            self.z_i = self.x_i + 1/self.rho*self.lambda_i
-            #print(self.z_i)
+            self.z_i = self.x_i + 1/self.rho*self.lambda_i #Stakeholders entry in sum
 
-            self.conn1.sendall(self.z_i.tobytes())
+            self.conn1.sendall(self.z_i.tobytes())  #Distribute z_i
             self.conn2.sendall(self.z_i.tobytes())
-            print("z_i")
-            print(type(self.z_i))
-            print(type(self.z_i[1]))      
-            print(len(self.z_i))    
-            print(len(self.z_i.tobytes()))
 
-            self.z_2 = np.frombuffer(self.conn1.recv(384), dtype=self.z_i.dtype)
-            #print(self.z_2)
-            self.z_3 = np.frombuffer(self.conn2.recv(384), dtype=self.z_i.dtype)
-            #print(self.z_3)
-            
-
+            self.z_2 = np.frombuffer(self.conn1.recv(8*self.Nc*self.N_q), dtype=self.z_i.dtype) #Recive other z_i's
+            self.z_3 = np.frombuffer(self.conn2.recv(8*self.Nc*self.N_q), dtype=self.z_i.dtype)
 
             self.z_tilde = self.z_i + self.z_2+ self.z_3
             self.lambda_i_tilde = self.lambda_i + self.rho*(self.z - self.z_tilde)
             self.z = self.z - 1/(self.N_s + 1)*(self.z - self.z_tilde)
             self.lambda_i = self.lambda_i -1/(self.N_s + 1)*(self.lambda_i - self.lambda_i_tilde) 
             ### END ADMM 
-
             
             ### BEGIN find rho
             if(k<=self.N_vary_rho):
-                self.conn1.sendall(self.x_i.tobytes())
+                self.conn1.sendall(self.x_i.tobytes())  #Distribute x_i for residual calculation 
                 self.conn2.sendall(self.x_i.tobytes())
-                print("x_i")
-                print(type(self.x_i)) 
-                print(type(self.x_i[1])) 
-                print(len(self.x_i))    
-                print(len(self.x_i.tobytes()))
-                    
 
-                self.x_2 = np.frombuffer(self.conn1.recv(384), dtype= self.x_i.dtype)
-                self.x_3 = np.frombuffer(self.conn2.recv(384), dtype= self.x_i.dtype)
+                self.x_2 = np.frombuffer(self.conn1.recv(8*self.Nc*self.N_q), dtype= self.x_i.dtype) #Recive x_i's for reidual calculation
+                self.x_3 = np.frombuffer(self.conn2.recv(8*self.Nc*self.N_q), dtype= self.x_i.dtype)
 
                 self.x_bar_old = self.x_bar
                 self.x_bar = self.x_i + self.x_2 + self.x_3
@@ -92,4 +75,4 @@ class ADMM_optimiser_WDN:
                 print(self.rho)
             ### End find rho
             
-        return self.x_i[:2]       
+        return self.x_i[:2]       #Return actuation commands

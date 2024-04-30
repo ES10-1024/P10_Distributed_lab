a2d1cca91ee69f7aba389c6f01918187cbe6bcf0
LauLauridsen <lau.lauridsen@hotmail.com>
2024-04-30 13:46:23 +0200

    Example code for TCP communication

--- TCP example/client2.py
+++ TCP example/client2.py
@@ -1,47 +1,26 @@
-# echo-client.py
-
-"""
-Test of tcp networking in smart water infastructure lab at AAU
-
-Author: Lau Lauridsen
-"""
-
+#Client
 import socket
-import struct
+
 import time
 import numpy as np
 import pickle
 
-#HOST = "192.168.100.24"  # The server's hostname or IP address
-HOST = "127.0.0.1"
-PORT = 6644  # The port used by the server
-message = 5.2578
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect((HOST, PORT))
+HOST = "127.0.0.1"  #Servers IP
+PORT = 6644         #Servers port
 
-"""
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)         #Setup TCP socket
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     #If port is in use, steal the port
+s.connect((HOST, PORT))     #Connect to server
+
 
 while True:
-    s.sendall(struct.pack('f',message))
-    print("Message sendt")
-    data = s.recv(1024)
-    returned_message = struct.unpack('f',data)[0]
-    
-    print("Received",returned_message)
-    print(returned_message)
-    time.sleep(20)
-"""
-
-while True:
-    a= np.array([5,7,9])
-    s.sendall(a.tobytes())
+    a= np.array([5,7,9])    #Data to send
+    s.sendall(a.tobytes())  #Pack data to bytes
     print("Message sent")
-    data =0
-    data = s.recv(1024)
-    print(data)
-    returned_message = np.frombuffer(data,dtype=a.dtype)
+    retuned_data = s.recv(1024)
+    print(retuned_data)
+    returned_message = np.frombuffer(retuned_data, dtype=a.dtype)   #Unpack data
     
     print("Received",returned_message)
     time.sleep(60)
